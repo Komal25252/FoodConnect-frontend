b23d097bd1b2e1225af96f6a294ad34909c3ead1
Komal Mishra <122214487+Komal25252@users.noreply.github.com>
2025-12-02 02:08:51 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+ base: './',                   // relative assets paths
+  build: {
+    outDir: '.',                // write build files into the client root
+    emptyOutDir: false,         // be careful; prevents wiping client sources if you want to keep them
+    assetsDir: 'assets',        // default, can keep
+  },
   plugins: [react()],
   server: {
     port: 5173, // default Vite port
